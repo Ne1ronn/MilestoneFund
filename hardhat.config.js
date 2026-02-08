c6220950e32c254a44b26d58a90f05abf629c038
Yesset Kozhakhmet <esetkozcahmet87@gmail.com>
2026-02-08 19:23:23 +0500

    move network configuration and private keys to environment variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
     solidity: "0.8.20",
     networks: {
         sepolia: {
-            url: process.env.SEPOLIA_RPC_URL || "",
-            accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+            url: SEPOLIA_RPC_URL,
+            accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
         },
     },
 };
