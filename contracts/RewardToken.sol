7e96884f488f3df76a74f60c7b4278f5a6307d26
isk0nka-dot <isk0nka@bk.ru>
2026-02-08 21:10:16 +0500

    Enhance mint function with address and amount validation
    Add checks to prevent minting to zero address or with zero amount

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -5,12 +5,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RewardToken is ERC20, Ownable {
+
     constructor()
         ERC20("Reward Token", "RWT")
         Ownable(msg.sender)
     {}
 
     function mint(address to, uint256 amount) external onlyOwner {
+        require(to != address(0), "Zero address");
+        require(amount > 0, "Zero amount");
+
         _mint(to, amount);
     }
 }
